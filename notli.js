ac28b7190498f51e7c48601dfd8566fb38f5b3e7
copperion302 <emily.huang.0302@gmail.com>
2023-05-18 18:51:19 +0800

    第一頁改正

---
+++ notli.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#direction").click(
+        function(){
+            $("#direction").css("opacity","0");
+            $("#ans").css("opacity","1");
+        })
+
+ 
+           
+    
+    })
