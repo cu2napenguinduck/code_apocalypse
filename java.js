f903cf0b6f64cd8d9310d038a2018307cee903a5
lifuya <fuhyahli@gmail.com>
2023-05-21 22:21:38 +0800

    更新

--- java.js
+++ java.js
@@ -38,6 +38,15 @@ $(".openingdoor").click(
         $(".yes").css("opacity","1");
         $(".no").css("opacity","1");
     }
-)
+);
+$(".face").click(
+    function(){
+        $(".myborder").css("opacity","1");
+        $(".q1").css("opacity","1");
+        $(".q2").css("opacity","1");
+        $(".judge").css("opacity","0.4");
+        $(".face").css("opacity","0");
+    }   
+);
 
 });
