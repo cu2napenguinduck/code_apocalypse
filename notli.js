2713924a02408343c676238f2a63f9eb3e62d9b3
copperion302 <emily.huang.0302@gmail.com>
2023-05-22 18:05:17 +0800

    更新3-2

--- notli.js
+++ notli.js
@@ -3,9 +3,24 @@ $(document).ready(function(){
         function(){
             $("#direction").css("opacity","0");
             $("#ans").css("opacity","1");
-        })
-
- 
+        });
+        
+    $(".ch1").click(
+        function(){
+            $(".ch1").css("opacity","0.2");
+            $(".white").css("opacity","0.5");
+            $(".border").css("opacity","1");
+            $(".judgeq").css("opacity","1");
+            $(".judgeq").css("z-index","4");
+            $(".border").css("z-index","3");
+            $(".white").css("z-index","1");
+            $(".ch1").css("z-index","2");
+            $(".ans1").css("opacity","1");
+            $(".ans2").css("opacity","1");
+            $(".ans1").css("z-index","5");
+            $(".ans2").css("z-index","6");
+        });
+     
            
     
-    })
+    });
